380bdac24393d1c6688215141212f5eb7d068076
tracht <marija.trachtenberg@gmail.com>
2020-04-27 16:13:29 +0100

    created views directory and moved cat image to index.erb file within views folder

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+# In our case, app.rb will be for routing (or controlling) concerns, and we will use another file for our presentation (or view) concerns.
 
 set :session_secret, 'super secret'
 
@@ -10,12 +11,10 @@ get '/secret' do
   "This is a secret page"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
-end
-
 get '/home' do
   "This is a home page"
+end
+
+get '/cat' do
+  erb(:index)
 end
